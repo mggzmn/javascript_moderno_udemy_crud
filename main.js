2c07c3c0df651bc5e15c4f749b969c78318e869f
Mariana Guzmán <mggzmn91@gmail.com>
2023-01-30 12:20:22 -0600

    Structure files and store

--- main.js
+++ main.js
@@ -12,12 +12,10 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+   
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('.card'))
