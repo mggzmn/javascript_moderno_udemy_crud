4289c1d924e52dd7ff2216a33fe5976f72b3a7bd
Mariana Guzmán <mggzmn91@gmail.com>
2023-01-31 14:48:28 -0600

    Delete with reload

--- src/users/store/users-store.js
+++ src/users/store/users-store.js
@@ -36,7 +36,10 @@ const onUserChanged = (updatedUser) => {
 }
 const reloadPage = async () => {
     const users = await loadUsersByPage(state.currentPage );
-    if (users.length === 0) return;
+    if (users.length === 0) {
+        await loadPreviousPage();
+        return
+    }
    
     state.users = users;
 }
